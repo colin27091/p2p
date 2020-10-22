a4d6f66136d0c6bf8f26239f3fa22810ee6f06c1
colin27091 <colinregourd@gmail.com>
2020-10-22 08:17:39 +0200

    form

---
+++ src/Form.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Form extends JFrame{
+    private JPanel panel1;
+
+    public Form(){
+        this.setContentPane(panel1);
+        this.setSize(new Dimension(200,200));
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-public class Main {
+import javax.swing.*;
+
+public class Main{
     public static void main(String[] args) {
-        System.out.println(args.toString());
+
+        new Form();
+
     }
 }
